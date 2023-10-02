932602cca4add9f2d2c858556df36ef63b368ca3
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2023-10-02 18:37:32 +0200

    Commit

--- src/main/java/org/listadodirectorio/ListadoDirectorio.java
+++ src/main/java/org/listadodirectorio/ListadoDirectorio.java
@@ -7,6 +7,8 @@ import java.util.*;
 public class ListadoDirectorio {
 
     public static void main(String[] args) {
+        final String CAD_FOR_CAB = "%-5s %-40s %14s  %-9s  %s\n";
+        final String CAD_FOR_FIL = "%-5s %-40s %14d  %-9s  %s\n";
         String ruta = ".";
         if (args.length >= 1) ruta = args[0];
         File fich = new File(ruta);
@@ -15,17 +17,17 @@ public class ListadoDirectorio {
         } else {
             if (fich.isFile()) {
                 System.out.println(ruta + " es un fichero.");
-                System.out.printf("%-5s %-40s %14s  %-9s  %s\n", "TIPO", "NOMBRE", "TAMAÑO(Bytes)", "PERMISOS", "FECHA_MODIFICACIÓN");
-                System.out.printf("%-5s %-40s %14d  %-9s  %s\n", "[-]", fich.getName(), fich.length(), damePermisos(fich), dameFecha(fich));
+                System.out.printf(CAD_FOR_CAB, "TIPO", "NOMBRE", "TAMAÑO(Bytes)", "PERMISOS", "FECHA_MODIFICACIÓN");
+                System.out.printf(CAD_FOR_FIL, "[-]", fich.getName(), fich.length(), damePermisos(fich), dameFecha(fich));
 
             } else {
                 System.out.println(ruta + " es un directorio. Contenidos: ");
-                System.out.printf("%-5s %-40s %14s  %-9s  %s\n", "TIPO", "NOMBRE", "TAMAÑO(Bytes)", "PERMISOS", "FECHA_MODIFICACIÓN");
+                System.out.printf(CAD_FOR_CAB, "TIPO", "NOMBRE", "TAMAÑO(Bytes)", "PERMISOS", "FECHA_MODIFICACIÓN");
                 File[] ficheros = fich.listFiles(); // Ojo, ficheros o directorios
                 for (File f : ficheros) {
                     String textoDescr = f.isDirectory() ? "[/]" : f.isFile() ? "[_]" : "?";
                     //String fechaModif = (new Date(f.lastModified()).toString());
-                   System.out.printf("%-5s %-40s %14d  %-9s  %s\n", textoDescr, f.getName(), f.length(), damePermisos(f), dameFecha(f));
+                   System.out.printf(CAD_FOR_FIL, textoDescr, f.getName(), f.length(), damePermisos(f), dameFecha(f));
                 }
             }
         }
